2bdf183b38b63bcc3c00ecdc24659141e9a0d737
Владислав Тищенко <vladislavtishenko@MacBook-Air-Vladislav.local>
2023-04-23 12:23:53 +0300

    добавил анимации + wow.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,22 @@ import * as flsFunctions from "./modules/functions.js";
 
 document.addEventListener("DOMContentLoaded", () => {
 	flsFunctions.isWebp();
+
+	function animateLine() {
+		const line = document.querySelector(".header__wrapper-line");
+		let width = 0;
+
+		const id = setInterval(() => {
+			if (width <= 100) {
+				width += 1;
+				line.style.width = `${width}%`;
+			} else {
+				clearInterval(id);
+			}
+
+		}, 10);
+		
+	}
+
+	animateLine();
 });
